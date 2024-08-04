81863d9eafeede650bb41bd1744b636bb6e455c1
einarr <einarr.net@gmail.com>
2024-08-04 22:02:28 +1200

    created stage1.sh and wrote file header in comments

---
+++ stage1.sh
@@ -0,0 +1,15 @@
+#########################################################################
+#									#
+# EINARCH STAGE 1 INSTALL SCRIPT					#
+#									#
+# This script was designed to automate installing EinArch		#
+# EinArch is my own personal build of Arch Linux			#
+# Stage 1 script will format the freshly partitioned drives		#
+# into the correct file system, Mount the drive partitions,		#
+# Install the packages "linux", "linux-firmware", "base",		#
+# "base-devel" "neovim" and "git"					#
+# It will also create the file system tables and chroot into the system	#
+#									#
+#########################################################################
+
+#!/bin/bash
