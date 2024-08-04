d6f12e1aeffec8ce15e3d77a731374352b4a3d44
einarr <einarr.net@gmail.com>
2024-08-05 07:40:23 +1200

    created stage2.sh file and added header in comments

---
+++ stage2.sh
@@ -0,0 +1,14 @@
+#################################################################
+#								#
+# EINARCH STAGE 2 INSTALL SCRIPT				#
+#								#
+# This script was designed to automate installing EinArch	#
+# EinArch is my own personal build of Arch Linux		#
+# The stage 2 script will carry on from where stage 1 left off	#
+# Completing the Arch Linux base installation allowing the	#
+# user to reboot into a working system with a tty		#
+#################################################################
+
+#!/bin/bash
+
+
